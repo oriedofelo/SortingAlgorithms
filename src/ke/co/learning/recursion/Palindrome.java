15aee0cb6aa8803a92416404c3f1c99804d6afe0
felix.ojiem <oriedofelo@gmail.com>
2017-09-04 10:11:10 +0300

    Palindrome, yah!

---
+++ src/ke/co/learning/recursion/Palindrome.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ke.co.learning.recursion;
+
+/**
+ *
+ * @author felix.ojiem
+ */
+public class Palindrome {
+	private boolean calculator(String word, int beginAt, int endAt) {
+		if (word.length() <= 1) {
+			return true;
+		} else {
+			char chars[] = word.toCharArray();
+			if (chars[0] == chars[endAt]) {
+				String substring = word.substring(1, endAt--);
+				return calculator(substring, beginAt, substring.length() - 1);
+
+			} else {
+				return false;
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Palindrome pal = new Palindrome();
+		String theString = "rotor";
+		boolean result = pal.calculator(theString, 0, theString.length() - 1);
+		System.err.println("Is palindrome: " + result);
+	}
+}
